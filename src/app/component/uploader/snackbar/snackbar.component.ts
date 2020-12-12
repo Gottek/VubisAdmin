9214ecd113b6526925f331e5de75f5046849c0b9
Khalifa Sami <klfsami08@gmail.com>
2020-12-12 19:02:07 +0100

    okok

---
+++ src/app/component/uploader/snackbar/snackbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-snackbar',
+  templateUrl: './snackbar.component.html',
+  styleUrls: [`
+    .example-pizza-party {
+      color: hotpink;
+    }
+  `],
+})
+export class SnackbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
